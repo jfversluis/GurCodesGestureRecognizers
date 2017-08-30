ef0820dd589c463770c22e43b4a117de153dd3fd
Gerald Versluis <gerald@verslu.is>
2017-08-30 14:59:39 +0200

    Initial commit

---
+++ GurCodesGestureRecognizers/GurCodesGestureRecognizersPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Xamarin.Forms;
+
+namespace GurCodesGestureRecognizers
+{
+    public partial class GurCodesGestureRecognizersPage : ContentPage
+    {
+        public GurCodesGestureRecognizersPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            DisplayAlert("Hi!", "This is a message", "OK");
+        }
+
+        private void Handle_Tapped(object sender, System.EventArgs e)
+        {
+            DisplayAlert("Hi!", "This is a message", "OK");
+        }
+
+        void Handle_PinchUpdated(object sender, Xamarin.Forms.PinchGestureUpdatedEventArgs e)
+        {
+            if (e.Status == GestureStatus.Started)
+                DisplayAlert("Hi!", "This is a message", "OK");
+        }
+    }
+}
